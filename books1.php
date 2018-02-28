e191a2bfc5f0cd144619e4f439f591e119e90a06
Andrew Wright <a.wright@kainos.com>
2018-02-28 10:54:10 +0000

    Pulling in page design from gov.uk

--- books1.php
+++ books1.php
@@ -28,52 +28,54 @@
         </div>
       </div>
     </header>
-    <div class="container">
-      <div class="header hidden-xs">
-        <ul class="nav nav-pills pull-right">
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Standard Login<b class="caret"></b></a>
-            <ul class="nav dropdown-menu">
-              <li><a href="login1.php">Vulnerable</a></li>
-              <li><a href="login2.php">Secure</a></li>
-            </ul>
-          </li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Numeric Login<b class="caret"></b></a>
-            <ul class="nav dropdown-menu">
-              <li><a href="login3.php">Vulnerable</a></li>
-              <li><a href="login4.php">Secure</a></li>
-            </ul>
-          </li>
-          <li class="active dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Search<b class="caret"></b></a>
-            <ul class="nav dropdown-menu">
-              <li><a href="books1.php">Vulnerable</a></li>
-              <li><a href="books2.php">Secure</a></li>
-            </ul>
-          </li>
-        </ul>
-		<h3 class="text-muted"><a href="index.php">SQL-Injection Demo</a></h3>
-      </div>
-      <?php include("mobile-navbar.php"); ?>
-      
-      <h3 class="text-center"><span class="label label-danger">
-Vulnerable Search</span></h3><br>
-      
-      <div class="row">
-        <div class="col-sm-10">
-          <form class="form-inline" role="form" action="books1.php" method="GET">
-            <div class="form-group">
-              <label class="sr-only" for="exampleInputEmail2">Book title</label>
-              <input type="text" name="title" class="form-control" placeholder="Book title">
-            </div>
-            <div class="form-group">
-              <label class="sr-only" for="exampleInputPassword2">Book author</label>
-              <input type="text" name="author" class="form-control"placeholder="Book author">
-            </div>
-            <button type="submit" class="btn btn-success">Search</button>
-          </form>
-        </div>
+    <div id="global-header-bar"></div>
+    <div id="wrapper" class="local_transaction service">
+    <div class="govuk-breadcrumbs " data-module="track-click">
+      <ol>
+
+        <li class="">
+            <a data-track-category="breadcrumbClicked" data-track-action="1" data-track-label="/" data-track-options="{&quot;dimension28&quot;:&quot;3&quot;,&quot;dimension29&quot;:&quot;Home&quot;}" class="" aria-current="false" href="/">Home</a>
+        </li>
+
+        <li class="">
+            <a data-track-category="breadcrumbClicked" data-track-action="2" data-track-label="/browse/housing-local-services" data-track-options="{&quot;dimension28&quot;:&quot;3&quot;,&quot;dimension29&quot;:&quot;Housing and local services&quot;}" class="" aria-current="false" href="/browse/housing-local-services">Housing and local services</a>
+        </li>
+
+        <li class="">
+            <a data-track-category="breadcrumbClicked" data-track-action="3" data-track-label="/browse/housing-local-services/local-councils" data-track-options="{&quot;dimension28&quot;:&quot;3&quot;,&quot;dimension29&quot;:&quot;Local councils and services&quot;}" class="" aria-current="false" href="/browse/housing-local-services/local-councils">Local councils and services</a>
+        </li>
+      </ol>
+    </div>
+    <div class="grid-row">
+        <main id="content" class="" role="main">
+          <header class="page-header">
+			<div class="pub-c-title pub-c-title--bottom-margin">
+			  <h1 class="pub-c-title__text">
+				Search the local library catalogue
+			  </h1>
+			</div>
+		  </header>
+		<div class="article-container group">
+			<div class="content-block">
+			  <div class="inner">
+			  <form class="form-inline" role="form" action="books1.php" method="GET">
+				<div class="form-group">
+				  <label class="sr-only" for="exampleInputEmail2">Book title</label>
+				  <input type="text" name="title" class="form-control" placeholder="Book title">
+				</div>
+				<div class="form-group">
+				  <label class="sr-only" for="exampleInputPassword2">Book author</label>
+				  <input type="text" name="author" class="form-control"placeholder="Book author">
+				</div>
+				<button type="submit" class="button" role="button">Search</button>
+			  </form>
+			  </div>
+			</div>
+			<div class="meta-data group">
+			<p class="modified-date">Last updated: 27 February 2018</p>
+		</div>
+	  </div>
+        </main>
         <div class="col-sm-2">
           <span class="visible-xs">&nbsp;</span>
           <a href="books1.php?all=1"><button type="button" class="btn btn-info">All books</button></a>
